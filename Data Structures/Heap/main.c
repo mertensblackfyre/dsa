397f92c4641046af11bb00901022a3f5501ae2b2
mertensblackfyre <72759681+mertensblackfyre@users.noreply.github.com.>
2025-06-19 19:17:33 +0300

    Heap

--- Data Structures/Heap/main.c
+++ Data Structures/Heap/main.c
@@ -1,4 +1,3 @@
-
 #include <math.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -7,22 +6,37 @@
 
 #define N 20
 
+typedef struct Tuple {
+  int *min;
+  int pos;
+} Tuple;
+
 typedef struct Heap {
-  int data[20];
+  int data[N];
   size_t size;
 } Heap;
 
-int push(int value, Heap *data);
+void push(int value, Heap *data);
 void print(Heap *data);
-int main() {
 
+void pop(Heap *data);
+Tuple *min(int *lchild, int *rchild, int lpos, int rpos);
+
+int main() {
   Heap *data = malloc(sizeof(Heap));
   data->size = 1;
   push(16, data);
   push(21, data);
-  push(26, data);
   push(14, data);
   push(19, data);
+  push(26, data);
+  push(19, data);
+
+  push(68, data);
+  push(65, data);
+  push(30, data);
+
+  pop(data);
 
   print(data);
   return 0;
@@ -30,27 +44,61 @@ int main() {
 
 void print(Heap *data) {
   for (size_t i = 0; i < data->size; i++) {
+    if (data->data[i] == 0)
+      continue;
     printf("%d -> ", data->data[i]);
   };
-
   printf("\n");
 };
-int push(int value, Heap *data) {
+
+void push(int value, Heap *data) {
   data->data[data->size++] = value;
 
-  int i = data->size;
-  int parent = floor(i / 2);
-  while (data->data[i] < data->data[parent]) {
-    if (data->data[parent] == 0 || data->data[i] == 0) {
-      break;
-    }
+  int i = data->size - 1;
+  while (data->data[i] < data->data[i / 2]) {
     int tmp = data->data[i];
-    data->data[i] = data->data[parent];
-    data->data[parent] = tmp;
+    data->data[i] = data->data[i / 2];
+    data->data[i / 2] = tmp;
 
-    i = floor(i / 2);
-    parent = floor(i / 2);
+    i = i / 2;
   };
+};
 
-  return 1;
+Tuple *min(int *lchild, int *rchild, int lpos, int rpos) {
+  Tuple *t = malloc(sizeof(Tuple));
+  if (lchild <= rchild) {
+    t->min = lchild;
+    t->pos = lpos;
+  } else {
+    t->min = rchild;
+    t->pos = rpos;
+  }
+  return t;
+};
+void pop(Heap *data) {
+  if (data->size == 0)
+    return;
+
+  int i = 1;
+  int lchild = 2 * i;
+  int rchild = 2 * i + 1;
+
+  data->data[1] = data->data[data->size - 1];
+  data->data[data->size - 1] = 0;
+
+  Tuple *min_tupe =
+      min(&data->data[lchild], &data->data[rchild], lchild, rchild);
+  while (*min_tupe->min <= data->data[i]) {
+    int tmp = *min_tupe->min;
+    *min_tupe->min = data->data[i];
+    data->data[i] = tmp;
+ 
+/*
+    i = min_tupe->pos;
+
+    lchild = 2 * i;
+    rchild = 2 * i + 1;
+    min_tupe = min(&data->data[lchild], &data->data[rchild], lchild, rchild);
+  */
+    }
 };
